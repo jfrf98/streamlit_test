71ea1a160c804eeec43bc5093455ec83a31ae7c7
Francisco RoFa <rodriguezfabre.josefrancisco@gmail.com>
2025-06-19 13:32:08 -0600

    number of trials

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
+from turtledemo.penrose import start
+
 import streamlit as st
 
 st.header("Lanzar una moneda")
+
+number_of_trials = st.slider("¿Numero de intentos?", 1 , 1000, 10)
+start_botton = st.button("Ejecutar")
+
+if start_botton:
+    st.write(f"Experimento con {number_of_trials} intentos en curso.")
+
 st.write("Esta aplicacion aun no es funcional. En construccion.")
